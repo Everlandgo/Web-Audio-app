5b25dc9aadba0d8480adf9b1b17dcd9e14e3ea37
Julie <songjungeon@gmail.com>
2023-11-01 16:24:23 +0800

    make buttons

--- recordAudio.js
+++ recordAudio.js
@@ -1,63 +1,39 @@
-let pausebtn; 
-let playbtn; 
-let stopbtn; 
+let pausebtn 
+let playbtn 
+let stopbtn
+let restartbtn
+let endbtn
+let loopbtn
+let recordbtn
 
 function setup(){
-    createCanvas(800, 800);
+    createCanvas(800, 600);
+    background('white');
 
-
+    pausebtn=createbtn('Pause',20,20); 
+    playbtn=createbtn('Play',120,20); 
+    stopbtn=createbtn('Stop',220,20); 
+    restartbtn=createbtn('Skip to start',320,20); 
+    endbtn=createbtn('Skip to end',420,20); 
+    loopbtn=createbtn('Loop',520,20); 
+    recordbtn=createbtn('record',620,20); 
+    
 
 }
-
-
 function mouseClicked(){
-    console.log(state);
-    if(getAudioContext().state !== 'running'){
-        getAudioContext().resume();
-    }
-    if(state === 0 && mic.enabled){
-        fill('pink');
-        textSize(30)
-        printtext='Recording';
-        backCol='red';
-        recorder.record(soundFile);
-        state++;
-    }
-    else if(state === 1){
-        backCol='blue';
-        printtext='Click to play and download';
-        fill(255,255,255);
-        recorder.stop();
-        state++;
-    }
-    else if(state === 2){
-        backCol='green';
-        printtext='click to record';
-        fill(255,255,255);
-        soundFile.play();
-        save(soundFile,'output.wav');
-        state=0;
-    }
+  
 
 }
 
-function draw() {
-    background(220);
-  
-    fill('pink');
-    rect(20, 20, 60, 40);
-    fill(0);
-    text('Pause', 30, 40); 
-  
-    fill('pink');
-    rect(100, 20, 60, 40);
-    fill(0);
-    text('Play', 130, 40);
-  
-    fill('pink');
-    rect(180, 20, 60, 40);
-    fill(0);
-    text('Stop', 210, 40);
-  }
-  
+function draw(){
+
+
+}
+
+function createbtn(text,X,Y){
+    var button =createButton(text); 
+    button.size(80,50);
+    button.position(X, Y);
+
+}
 
