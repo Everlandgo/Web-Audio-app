62a3fab19706003162719d72abd4717f5f479cfa
Julie <songjungeon@gmail.com>
2023-11-01 23:43:43 +0800

    implement template

--- recordAudio.js
+++ recordAudio.js
@@ -6,19 +6,20 @@ let endbtn
 let loopbtn
 let recordbtn
 
+let rect1; 
+let rect2; 
+let rect3; 
+let rect4; 
+let rect5; 
+let rect6; 
+
+
 function setup(){
-    createCanvas(800, 600);
-    background('white');
-
-    pausebtn=createbtn('Pause',20,20); 
-    playbtn=createbtn('Play',120,20); 
-    stopbtn=createbtn('Stop',220,20); 
-    restartbtn=createbtn('Skip to start',320,20); 
-    endbtn=createbtn('Skip to end',420,20); 
-    loopbtn=createbtn('Loop',520,20); 
-    recordbtn=createbtn('record',620,20); 
+    createCanvas(800, 800);
+    background('lightblue');
+    drawingbuttons();
+    drawbackrectangles();
     
-
 }
 function mouseClicked(){
   
@@ -30,10 +31,30 @@ function draw(){
 
 }
 
+function drawingbuttons(){
+
+    pausebtn=createbtn('Pause',20,20); 
+    playbtn=createbtn('Play',105,20); 
+    stopbtn=createbtn('Stop',195,20); 
+    restartbtn=createbtn('Skip to start',285,20); 
+    endbtn=createbtn('Skip to end',375,20); 
+    loopbtn=createbtn('Loop',465,20); 
+    recordbtn=createbtn('record',555,20); 
+
+}
+
+function drawbackrectangles(){
+    rect1=rect(20,90,200,300);
+    rect2=rect(245,90,300,400);
+    rect2=rect(575,90,150,200);
+
+}
+
 function createbtn(text,X,Y){
     var button =createButton(text); 
-    button.size(80,50);
+    button.size(70,50);
     button.position(X, Y);
 
 }
+
 
