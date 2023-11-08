1eda79e25f4e28bd8f43e9f2572c2baefe7c9fe6
Julie <songjungeon@gmail.com>
2023-11-08 19:35:46 +0800

    knob upgrade

--- knob.js
+++ knob.js
@@ -1,42 +1,52 @@
 class Knob {
-    constructor(x, y, r, angle,knobname) {
+    constructor(x, y, r, angle, minval,maxval) {
       this.x = x;
       this.y = y;
       this.r = r;
       this.angle = angle;
-      this.knobname=knobname;
       this.isdragging = false;
+      this.fill=255;
+      this.minval=minval;
+      this.maxval=maxval;
     }
   
     drawknob() {
-      fill(255);
-      strokeWeight(1);
-      translate(this.x, this.y);
-      rotate(this.angle);
-      circle(0, 0, this.r * 2);
-      line(0, 0, this.r, 0);
+        push(); 
+        fill(this.fill);
+        strokeWeight(1);
+        translate(this.x, this.y);
+        rotate(this.angle);
+        circle(0, 0, this.r * 2);
+        line(0, 0, this.r, 0);
+        pop(); 
     }
   
     updateknob() {
-      if (this.dragging) {
+      if (this.isdragging) {
         let dx = mouseX - this.x;
         let dy = mouseY - this.y;
         this.angle = atan2(dy, dx);
+        this.fill=color(244, 253, 175);
+        
       }
     }
   
     knobclicked() {
-      let d = dist(mouseX, mouseY, this.x, this.y);
-      if (d < this.r) {
-        this.dragging = true;
-        let dx = mouseX - this.x;
-        let dy = mouseY - this.y;
-        this.offsetAngle = atan2(dy, dx) - this.angle;
+        let d = dist(mouseX, mouseY, this.x, this.y);
+        if (d < this.r) {
+          this.isdragging = true;  
+        } else {
+          this.isdragging = false;
+          return false;
+        }
       }
-    }
-  
-    release() {
+      
+
+    knobReleased() {
       this.isdragging = false;
+      this.fill=255;
+
     }
+
   }
   
