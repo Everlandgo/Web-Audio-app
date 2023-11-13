9c73090e56342a2a76b9537cdbbbcc558b261f68
Julie <songjungeon@gmail.com>
2023-11-14 00:59:48 +0800

    tidy code and add further developments

--- sketch1.js
+++ sketch1.js
@@ -11,14 +11,16 @@ let reversebtn;
 let volumeslider;
 let LP_drywetslider;
 let LP_outputslider;
+let WAV_drywetslider;
+let WAV_outputslider;
+let DE_drywetslider;
+let DE_outputslider;
 let DY_drywetslider;
 let DY_outputslider;
 let RE_drywetslider;
 let RE_outputslider;
-let WAV_drywetslider;
-let WAV_outputslider;
 
-//knob
+//knobs
 let knobs = [];
 
 let frequencyknob = [];
@@ -32,23 +34,27 @@ let reverbdurknob = [];
 let decayknob = [];
 let distortionknob = [];
 let oversampleknob = [];
+let delaytimeknob = [];
 
+
+//audio
 let mic;
 let recorder;
 let soundFile;
 let state = 0;
-let fft;
-let offsetangle;
-let cutofffre;
+
+// apply differnt filter
+let choosefilter;
+
+// effects 
+let lowpassfilter;
+let distortioneffect;
+let compressoreffect;
+let reverbeffect;
+let mastervolume;
 
 let islooping = false;
 let isreverse = false;
-
-let dynamicCompressor;
-let reverbeffect;
-let lowpassfilter;
-let distortion;
-let mastervolume;
 
 function preload() {
   soundFormats("mp3", "wav");
@@ -62,10 +68,12 @@ function setupfilters() {
   recorder = new p5.SoundRecorder();
   recorder.setInput(mic);
 
+  //effects
   soundFile = new p5.SoundFile();
   lowpassfilter = new p5.LowPass();
   distortioneffect = new p5.Distortion();
-  compressoreffect = new p5.Compressor();
+  delayeffect = new p5.Delay();
+  compressoreffect= new p5.Compressor();
   reverbeffect = new p5.Reverb();
   mastervolume = new p5.Gain();
   Inputfft = new p5.FFT();
@@ -74,6 +82,7 @@ function setupfilters() {
   myaudiofile.disconnect();
   lowpassfilter.disconnect();
   distortioneffect.disconnect();
+  delayeffect.disconnect();
   compressoreffect.disconnect();
   reverbeffect.disconnect();
   mastervolume.disconnect();
@@ -82,7 +91,8 @@ function setupfilters() {
 
   myaudiofile.connect(lowpassfilter);
   lowpassfilter.connect(distortioneffect);
-  distortioneffect.connect(compressoreffect);
+  distortioneffect.connect(delayeffect);
+  delayeffect.connect(compressoreffect);
   compressoreffect.connect(reverbeffect);
   reverbeffect.connect(mastervolume);
   mastervolume.connect();
@@ -95,10 +105,12 @@ function setup() {
   let x = (windowWidth - width) / 2;
   let y = (windowHeight - height) / 2;
   canvas.position(x, y);
+
   setupfilters();
   drawknobs();
   drawbuttons();
   drawslider();
+  identifyFilter();
 }
 
 function draw() {
@@ -106,6 +118,8 @@ function draw() {
   background("#e4f0c2");
   drawingframe();
   writelabels();
+  choosefilter.changed(filterchanged);
+
   for (let knob of knobs) {
     knob.drawknob();
     fill(0);
@@ -113,6 +127,7 @@ function draw() {
     textSize(11);
     text(knob.value.toFixed(1), knob.x, knob.y + 40);
   }
+
   drawspectrum();
 
   if (myaudiofile.currentTime().toFixed(2) == myaudiofile.duration().toFixed(2)) {
@@ -126,7 +141,10 @@ function drawingframe() {
   rect(20, 420, 190, 370);
   rect(235, 100, 270, 400);
   rect(235, 510, 270, 280);
-  rect(535, 100, 150, 200);
+  rect(850, 100, 200, 200);
+  rect(535, 100, 300, 200);
+
+  //spectrum rectangles
   push();
   fill("#e2e1e8");
   rect(535, 320, 640, 230);
@@ -136,46 +154,14 @@ function drawingframe() {
   fill(0);
   textSize(20);
   textFont("Times new roman");
-  text("Low-pass filter", 60, 130);
+  text("filter", 150, 135);
   text("Dynamic compressor", 290, 130);
-  text("Master \nvolume", 580, 130);
+  text("Master\nvolume", 925, 130);
   text("Reverb", 80, 450);
   text("Waveshaper distortion", 290, 540);
+  text("Delay", 660, 130);
   text("Spectrum In ", 540, 340);
   text("Spectrum Out", 540, 580);
-}
-
-function drawspectrum() {
-  push();
-  let spectrumIN = Inputfft.analyze();
-  noStroke();
-  fill(255, 0, 255);
-
-  let boxX = 540;
-  let boxY = 345;
-  let boxwidth = 2800;
-  let boxheight = 200;
-
-  for (let i = 0; i < spectrumIN.length; i++) {
-    let x = map(i, 0, spectrumIN.length, boxX, boxX + boxwidth);
-    let h = -boxheight + map(spectrumIN[i], 0, 255, boxheight, 0);
-    rect(x, boxY + boxheight, boxwidth / spectrumIN.length, h);
-  }
-
-  let boxX2 = 540;
-  let boxY2 = 585;
-  let boxwidth2 = 630;
-  let boxheight2 = 200;
-
-  let spectrumOUT = Outputfft.analyze();
-  noStroke();
-  fill(0);
-  for (let i = 0; i < spectrumOUT.length; i++) {
-    let x = map(i, 0, spectrumOUT.length, boxX2, boxX2 + boxwidth2);
-    let h = -boxheight + map(spectrumOUT[i], 0, 255, boxheight2, 0);
-    rect(x, boxY2 + boxheight2, boxwidth2 / spectrumOUT.length, h);
-  }
-  pop();
 }
 
 function drawbuttons() {
@@ -190,20 +176,24 @@ function drawbuttons() {
 }
 
 function drawslider() {
-  volumeslider = createslider(0, 20, 3, 1, 665, 220, "volumeslider");
+  volumeslider = createslider(0, 20, 3, 1, 1010, 220, "volumeslider");
   LP_drywetslider = createslider(0, 1, 0.5, 0.1, 140, 330, "LP_drywetslider");
   LP_outputslider = createslider(0, 1, 0.5, 0.1, 225, 330, "LP_outputslider");
+  WAV_drywetslider = createslider(0, 1, 0.5, 0.1, 380, 730, "WAV_drywetslider");
+  WAV_outputslider = createslider(0, 1, 0.5, 0.1, 480, 730, "WAV_outputslider");
+  DE_drywetslider = createslider(0, 1, 0.5, 0.1, 780, 220, "DE_drywetslider");
+  DE_outputslider= createslider(0, 1, 0.5, 0.1, 860, 220, "DE_outputslider");
   DY_drywetslider = createslider(0, 1, 0.5, 0.1, 380, 430, "DY_drywetslider");
   DY_outputslider = createslider(0, 1, 0.5, 0.1, 480, 430, "DY_outputslider");
   RE_drywetslider = createslider(0, 1, 0.5, 0.1, 140, 720, "RE_drywetslider");
   RE_outputslider = createslider(0, 1, 1, 0.1, 225, 720, "RE_outputslider");
-  WAV_drywetslider = createslider(0, 1, 0.5, 0.1, 380, 730, "WAV_drywetslider");
-  WAV_outputslider = createslider(0, 1, 0.5, 0.1, 480, 730, "WAV_outputslider");
 
   lowpassfilter.drywet(LP_drywetslider.value());
   lowpassfilter.amp(LP_outputslider.value());
   distortioneffect.drywet(WAV_drywetslider.value());
   distortioneffect.amp(WAV_outputslider.value());
+  delayeffect.drywet(DE_drywetslider.value());
+  delayeffect.amp(DE_outputslider.value());
   compressoreffect.drywet(DY_drywetslider.value());
   compressoreffect.amp(DY_outputslider.value());
   reverbeffect.drywet(RE_drywetslider.value());
@@ -236,18 +226,21 @@ function drawknobs() {
 
   distortionknob = new Knob(320, 605, 25, 0, 0, 0, 1, "distortionknob");
   knobs.push(distortionknob);
-  oversampleknob = new Knob(420, 605, 25, 0, 0, 0, 1, "oversampleknob");
+  oversampleknob = new Knob(420, 605, 25, 0, 0, 0, 4, "oversampleknob");
   knobs.push(oversampleknob);
+
+  delaytimeknob = new Knob(610, 200, 25, 0, 0, 0, 1, "delaytimeknob");
+  knobs.push(delaytimeknob);
 
   lowpassfilter.set(frequencyknob.value,resonanceknob.value);
   distortioneffect.set(distortionknob.value, oversampleknob.value);
+  delayeffect.delayTime(delaytimeknob.value);
   compressoreffect.attack(attackknob.value);
   compressoreffect.knee(kneeknob.value);
   compressoreffect.release(releaseknob.value);
   compressoreffect.ratio(ratioknob.value);
   compressoreffect.threshold(thresholdknob.value);
   reverbeffect.set(reverbdurknob.value, decayknob.value, false);
-
 }
 
 function writelabels() {
@@ -262,27 +255,93 @@ function writelabels() {
   text("output \n level", 395, 660);
   text("dry/wet", 50, 660);
   text("output \n level", 140, 650);
-  text("  cutoff \nfreqency", 55, 160);
+  text("dry/wet", 50, 660);
+  text("output \n level", 780, 155);
+  text("dry/wet", 700, 160);
   text("resonance", 125, 175);
   text("attack", 260, 180);
   text("knee", 350, 180);
   text("release", 440, 180);
   text("ratio", 300, 270);
   text("threshold", 390, 270);
+  text("delay time", 585, 165);
   text(" reverb\nduration", 55, 480);
   text("decay\n rate", 140, 480);
   text("distortion\n amount", 290, 560);
   text("oversample", 390, 570);
+  text("  cutoff \nfreqency", 55, 160);
 
-  text(volumeslider.value(), 650, 230);
+
+  text(volumeslider.value(), 980, 230);
   text(LP_drywetslider.value(), 90, 330);
   text(LP_outputslider.value(), 175, 330);
+  text(WAV_drywetslider.value(), 330, 730);
+  text(WAV_outputslider.value(), 430, 730);
+  text(DE_drywetslider.value(), 730, 220);
+  text(DE_outputslider.value(), 810, 220);
   text(DY_drywetslider.value(), 330, 430);
   text(DY_outputslider.value(), 430, 430);
   text(RE_drywetslider.value(), 90, 720);
   text(RE_outputslider.value(), 175, 720);
-  text(WAV_drywetslider.value(), 330, 730);
-  text(WAV_outputslider.value(), 430, 730);
+}
+
+function drawspectrum() {
+  push();
+  let boxX = 540;
+  let boxY = 345;
+  let boxwidth = 2800;
+  let boxheight = 200;
+
+  let boxX2 = 540;
+  let boxY2 = 585;
+  let boxwidth2 = 630;
+  let boxheight2 = 200;
+
+  //Drawing spectrum for original sound 
+  let spectrumIN = Inputfft.analyze();
+  noStroke();
+  fill(255, 0, 255);
+  for (let i = 0; i < spectrumIN.length; i++) {
+    let x = map(i, 0, spectrumIN.length, boxX, boxX + boxwidth);
+    let h = -boxheight + map(spectrumIN[i], 0, 255, boxheight, 0);
+    rect(x, boxY + boxheight, boxwidth / spectrumIN.length, h);
+  }
+
+  //Drawing spectrum for processed sound 
+  let spectrumOUT = Outputfft.analyze();
+  noStroke();
+  fill(0);
+  for (let i = 0; i < spectrumOUT.length; i++) {
+    let x = map(i, 0, spectrumOUT.length, boxX2, boxX2 + boxwidth2);
+    let h = -boxheight + map(spectrumOUT[i], 0, 255, boxheight2, 0);
+    rect(x, boxY2 + boxheight2, boxwidth2 / spectrumOUT.length, h);
+  }
+  pop();
+}
+
+function identifyFilter(){
+  choosefilter = createSelect();
+  choosefilter.option('Low-pass');
+  choosefilter.option('High-pass');
+  choosefilter.option('Band-pass');
+  choosefilter.selected('Low-pass');
+  choosefilter.attribute('style', 'font-family: "Times New Roman", Times;');
+  choosefilter.style('font-size', '18px');
+  choosefilter.position(165, 120);
+  choosefilter.size(100);
+}
+
+function filterchanged(){
+  let selectedfilter= choosefilter.value();
+  if(selectedfilter=='Low-pass'){
+    lowpassfilter.setType("lowpass");
+  }
+  else if(selectedfilter=='High-pass') {
+    lowpassfilter.setType("highpass");
+  }
+  else if(selectedfilter=='Band-pass'){
+    lowpassfilter.setType("bandpass");
+}
 }
 
 function createbtn(text, X, Y, callfunction) {
@@ -331,9 +390,6 @@ function pausefunction() {
     pausebtn.style("background-color", "#1c81ed");
     print("Pause button clicked");
   }
-  if (soundFile.isPlaying()) {
-    soundFile.pause();
-  }
 }
 
 function playfunction() {
@@ -354,9 +410,6 @@ function stopfunction() {
     pausebtn.style("background-color", "white");
     myaudiofile.jump(0);
     print("stop button clicked");
-  }
-  if (soundFile.isPlaying()) {
-    soundFile.stop();
   }
 }
 
@@ -423,36 +476,18 @@ function reversefunction() {
   }
 }
 
-function changesliderval(sliderName) {
-  print("changesliderval");
-  if (sliderName == "volumeslider") {
-    mastervolume.amp(volumeslider.value());
-  } else if (sliderName === "LP_drywetslider" || sliderName === "LP_outputslider") {
-    lowpassfilter.drywet(LP_drywetslider.value());
-    lowpassfilter.amp(LP_outputslider.value());
-  } else if (sliderName === "DY_drywetslider" || sliderName === "DY_outputslider") {
-    compressoreffect.drywet(DY_drywetslider.value());
-    compressoreffect.amp(DY_outputslider.value());
-  } else if (sliderName === "RE_drywetslider" || sliderName === "RE_outputslider") {
-    reverbeffect.drywet(RE_drywetslider.value());
-    reverbeffect.amp(RE_outputslider.value());
-  } else if (sliderName === "WAV_drywetslider" || sliderName === "WAV_outputslider") {
-    distortioneffect.drywet(WAV_drywetslider.value());
-    distortioneffect.amp(WAV_outputslider.value());
-  }
-}
-
 function mousePressed() {
   for (let knob of knobs) {
     knob.knobclicked();
     if (knob.isdragging) {
       knob.updateknob();
       if (knob.name == "frequencyknob" || knob.name == "resonanceknob") {
-        print(frequencyknob.value,res=resonanceknob.value);
         lowpassfilter.set(frequencyknob.value,resonanceknob.value);
       } else if (knob.name == "reverbdurknob" || knob.name == "decayknob") {
-        print( reverbdurknob.value, decayknob.value);
         reverbeffect.set(reverbdurknob.value, decayknob.value, false);
+      } else if (knob.name == "delaytimeknob" ) {
+        print('delayyy knob ',delaytimeknob.value);
+        delayeffect.delayTime(delaytimeknob.value);
       } else if (
         knob.name == "attackknob" ||
         knob.name == "kneeknob" ||
@@ -466,9 +501,39 @@ function mousePressed() {
         compressoreffect.ratio(ratioknob.value);
         compressoreffect.threshold(thresholdknob.value);
       } else if (knob.name == "distortionknob" || knob.name == "oversampleknob") {
-        distortioneffect.set(distortionknob.value);
+        let oversample=0; 
+        if (oversampleknob.value === 0) {
+          oversample = 'none';
+        } else if (oversampleknob.value > 0 && oversampleknob.value < 2) {
+          oversample = '2x';
+        } else if (oversampleknob.value >= 2) {
+          oversample = '4x';
+        }
+        distortioneffect.set(distortionknob.value,oversample);
       }
     }
+  }
+}
+
+function changesliderval(sliderName) {
+  print("changesliderval");
+  if (sliderName == "volumeslider") {
+    mastervolume.amp(volumeslider.value());
+  } else if (sliderName === "LP_drywetslider" || sliderName === "LP_outputslider") {
+    lowpassfilter.drywet(LP_drywetslider.value());
+    lowpassfilter.amp(LP_outputslider.value());
+  } else if (sliderName === 'DE_drywetslider' || sliderName === 'DE_outputslider') {
+    delayeffect.drywet(DE_drywetslider.value());
+    delayeffect.amp(DE_outputslider.value());
+  } else if (sliderName === "DY_drywetslider" || sliderName === "DY_outputslider") {
+    compressoreffect.drywet(DY_drywetslider.value());
+    compressoreffect.amp(DY_outputslider.value());
+  } else if (sliderName === "RE_drywetslider" || sliderName === "RE_outputslider") {
+    reverbeffect.drywet(RE_drywetslider.value());
+    reverbeffect.amp(RE_outputslider.value());
+  } else if (sliderName === "WAV_drywetslider" || sliderName === "WAV_outputslider") {
+    distortioneffect.drywet(WAV_drywetslider.value());
+    distortioneffect.amp(WAV_outputslider.value());
   }
 }
 
